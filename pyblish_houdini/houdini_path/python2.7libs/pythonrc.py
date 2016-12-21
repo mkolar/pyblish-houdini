d31bcaf75361cefe1307204c8c7f17477b6f37ca
Milan Kolar <mail@mkolar.com>
2016-12-21 15:40:19 +0100

    register pyblish ui

--- pyblish_houdini/houdini_path/python2.7libs/pythonrc.py
+++ pyblish_houdini/houdini_path/python2.7libs/pythonrc.py
@@ -5,5 +5,9 @@ except ImportError as e:
     print "pyblish: Could not load integration: %s" % e
 
 else:
+    import pyblish.api
+    pyblish.api.register_gui("pyblish_qml")
+    pyblish.api.register_gui("pyblish_lite")
+
     import pyblish_houdini
     pyblish_houdini.setup()
